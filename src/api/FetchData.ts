8cf917a7afc6ab2ce9503f6823668015d983df1d
Jiaqi.Cai <jiaqi.cai@transfinder.com>
2019-07-22 08:13:31 +0800

    Change api base url.

--- src/api/FetchData.ts
+++ src/api/FetchData.ts
@@ -9,8 +9,8 @@ import { Configuration } from './LeaderboardData';
  * The theme name may be the indentifier.
  */
 // const baseUrl = 'http://184.73.190.18/TF-Leaderboard-API/tf-leaderboard/';
-// const baseUrl = 'http://dashboard.hq.transfinder.com/TF-Leaderboard-API/tf-leaderboard/';
-const baseUrl = 'http://localhost/TF-Leaderboard-API/tf-leaderboard/';
+const baseUrl = 'http://dashboard.hq.transfinder.com/TF-Leaderboard-API/tf-leaderboard/';
+// const baseUrl = 'http://localhost/TF-Leaderboard-API/tf-leaderboard/';
 const templateId = 1;
 
 // let mock_data: LeaderboardData = {
